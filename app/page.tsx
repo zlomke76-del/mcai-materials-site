151fbd7c9c4afaf168f1eaecf19d546828b25291
zlomke76-del <zlomke76@gmail.com>
2026-05-08 12:35:51 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,21 @@ export default function Home() {
     <main className="global-gpi-poster">
       <img
         src="/images/background_image.png"
-        alt="Global GPI Sustainable Materials Technology"
+        alt="Global GPI Background"
         className="global-gpi-image"
       />
+
+      <div className="earth-video-wrap">
+        <video
+          className="earth-video"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src="/video/earth_01.mp4" type="video/mp4" />
+        </video>
+      </div>
     </main>
   );
 }
